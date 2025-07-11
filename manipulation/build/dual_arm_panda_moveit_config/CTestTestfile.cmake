e0c4512f92e297c9b45a810f91b14445f9b42e4a
Ramu Kathi <ramukathi9491@gmail.com>
2025-07-11 08:10:12 +0000

    Added manipulation_ws files to ros2_dev_containers

---
+++ manipulation/build/dual_arm_panda_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/manipulation_ws/src/moveit_resources/dual_arm_panda_moveit_config
+# Build directory: /workspaces/manipulation_ws/build/dual_arm_panda_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ manipulation/build/moveit_resources_panda_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/manipulation_ws/src/moveit_resources/panda_description
+# Build directory: /workspaces/manipulation_ws/build/moveit_resources_panda_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
